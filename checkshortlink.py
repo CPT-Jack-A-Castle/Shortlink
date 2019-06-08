498957f4510af7320122a7587c8e1320061750d1
Akin <41711810+Akin5@users.noreply.github.com>
2019-06-08 12:42:01 +0700

    Tools untuk mencheck hasil shortlink mu :')

---
+++ checkshortlink.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import os, sys, time
+
+# example shortlink : https://tinyurl.com/y3rltmq9
+try:
+    import requests
+    from bs4 import BeautifulSoup
+except ImportError as e:
+    print("Module requests dan bs4 belom di install !!")
+    print("Tolong install dahulu sebelum menggunakan tools ini!!")
+
+class checkShortlink:
+    def __init__(self, url):
+        self.url = 'http://checkshorturl.com/expand.php'
+        self.session = requests.Session()
+        self.data = {
+            'url': url
+        }
+        try:
+            self.req = self.session.post(self.url, data=self.data, allow_redirects=True)
+        except:
+            print("ERROR: Jalankan ulang ini program!")
+            sys.exit(0)
+        self.soup = BeautifulSoup(self.req.text, features='html.parser')
+        self.author = self.soup.find_all('td')[19].get_text()
+        self.longURL = self.soup.find_all('td')[1].find('a').get('href')
+        self.shortURL = self.soup.find_all('td')[5].get_text()
+        self.delay = self.soup.find_all('td')[3].get_text()
+        self.title = self.soup.find_all('td')[13].get_text()
+        self.description = self.soup.find_all('td')[15].get_text()
+        self.keywords = self.soup.find_all('td')[17].get_text()
+        print("Author : {}".format(self.author))
+        print("Long URL : {}".format(self.longURL))
+        print("Short URL : {}".format(self.shortURL))
+        print("Delay : {}".format(self.delay))
+        print("Title : {}".format(self.title))
+        print("Description : {}".format(self.description))
+        print("Keywords : {}".format(self.keywords))
+checkShortlink('https://tinyurl.com/y3rltmq9')
